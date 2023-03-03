2928c2a1463a9e12a3793e9c83898a655ab50659
lcweb-011 <115338029+lcweb-011@users.noreply.github.com>
2023-03-03 17:29:43 +0530

    init

---
+++ index.js
@@ -0,0 +1,67 @@
+
+
+// window._wq = window._wq || [];
+// _wq.push({ id: "g1221bnz2y", onReady: function(video) {
+//   console.log("I got a handle to the video!", video);
+//   // video.mute();
+//   video.play();
+//   video.bind("silentplaybackmodechange", function (inSilentPlaybackMode) {
+//     console.log("Is 'Click For Sound' visible?", inSilentPlaybackMode ? "yes" : "no");
+//   });
+//   video.bind("end", function(t) {
+//     video.play();
+//     console.log("the video ended");
+//   });
+// }});
+// window._wq = window._wq || [];
+// _wq.push({ id: "jdwgwux9jd", onReady: function(video) {
+//   console.log("I got a handle to the video!", video);
+// //   video.mute();
+//   video.play();
+// video.bind("silentplaybackmodechange", function (inSilentPlaybackMode) {
+//     console.log("Is 'Click For Sound' visible?", inSilentPlaybackMode ? "yes" : "no");
+//   });
+//   video.bind("end", function(t) {
+//     video.play();
+//     console.log("the video ended");
+//   });
+// }});
+// window._wq = window._wq || [];
+// _wq.push({ id: "2ynquhdr9a", onReady: function(video) {
+//   console.log("I got a handle to the video!", video);
+// //   video.mute();
+//   video.play();
+// video.bind("silentplaybackmodechange", function (inSilentPlaybackMode) {
+//     console.log("Is 'Click For Sound' visible?", inSilentPlaybackMode ? "yes" : "no");
+//   });
+//   video.bind("end", function(t) {
+//     video.play();
+//     console.log("the video ended");
+//   });
+// }});
+
+window._wq = window._wq || [];
+_wq.push({ id: "xr8jcugi6j", onReady: function(video) {
+  console.log("I got a handle to the video!", video);
+//   video.mute();
+  video.play();
+video.bind("silentplaybackmodechange", function (inSilentPlaybackMode) {
+    console.log("Is 'Click For Sound' visible?", inSilentPlaybackMode ? "yes" : "no");
+  });
+  video.bind("end", function(t) {
+    video.play();
+    console.log("the video ended");
+  });
+}});
+
+
+
+// const myElement = document.getElementById("Vid1");
+
+// console.log(myElement)
+// myElement.addEventListener("click", Func = () => {
+//     console.log("CLicked");
+//     video.unmute()
+// })
+
+console.log("I got a handle to the video!", video);
